f685df2c698ca2aa34833680766678ac46260fab
Imavin08 <92112933+Imavin08@users.noreply.github.com>
2022-04-03 23:41:29 +0200

    Правки от ментора
    правки от ментора сделаны

--- js/task-01.js
+++ js/task-01.js
@@ -1,20 +1,10 @@
-const allItemEl = document.querySelectorAll("#categories");
+const allItemEl = document.querySelectorAll(".item");
 
-allItemEl.forEach((elem) => {
-  console.log(`Number of categories: ${elem.children.length}`);
-
-  // 1
-  const firstEl = elem.firstElementChild;
-  console.log(`Category: ${firstEl.firstElementChild.textContent}`);
-  console.log(`Elements: ${firstEl.lastElementChild.children.length}`);
-
-  // 2
-  const secondEl = firstEl.nextElementSibling;
-  console.log(`Category: ${secondEl.firstElementChild.textContent}`);
-  console.log(`Elements: ${secondEl.lastElementChild.children.length}`);
-
-  // 3
-  const lastEl = elem.lastElementChild;
-  console.log(`Category: ${lastEl.firstElementChild.textContent}`);
-  console.log(`Elements: ${lastEl.lastElementChild.children.length}`);
-});
+const count = (elements) => {
+  console.log(`Number of categories: ${elements.length}`);
+  elements.forEach((element) => {
+    console.log(`Category: ${element.firstElementChild.textContent}`);
+    console.log(`Elements: ${element.lastElementChild.childElementCount}`);
+  });
+};
+count(allItemEl);
